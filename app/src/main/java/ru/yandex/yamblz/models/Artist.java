94fe5019c2f747e159e0ce6e035eee1f6dd0d890
maksimn <maksim1989sovunion@yandex.ru>
2018-08-10 19:59:55 +0300

    Artist class model

---
+++ app/src/main/java/ru/yandex/yamblz/models/Artist.java
@@ -0,0 +1,11 @@
+package ru.yandex.yamblz.models;
+
+public class Artist {
+    public String artistName;
+    public int albums;
+    public int songs;
+    public String description;
+    public String photo;
+    public int birthYear;
+    public int deathYear;
+}
